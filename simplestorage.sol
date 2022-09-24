c1871ae35ca4bbe938c094f716c0e13a7ae03448
Marvish Subhas Chandra <mschandr@uci.edu>
2022-09-24 12:35:57 -0700

    Add files via upload

--- simplestorage.sol
+++ simplestorage.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.8;
 
 contract SimpleStorage {
     string public contactNumber = "1-866-278-5833";
+    string public fundraisingNumber = "1-800-457-2494"
     
     function store(uint256 SaintJude) public {
         contactNumber = SaintJude;
